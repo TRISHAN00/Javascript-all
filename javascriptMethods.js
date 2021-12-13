ef5bda697be8db6c067154c7b009722f008ba105
TRISHAN00 <trishansaha43@gmail.com>
2021-12-13 12:05:15 +0600

    slice method

---
+++ javascriptMethods.js
@@ -0,0 +1,25 @@
+// String Length
+const name = "trisha"
+
+
+
+// Javascript Object
+const players = {
+    name : 'trishan',
+    age : 26,
+    country : 'bangladesh',
+    slice : function(start, end) {
+        console.log('slice');
+    },
+    substring : function(start, end) {
+        console.log('substring');
+    },
+    substr : function() {
+        console.log('substr');
+    }
+}
+
+
+const fruits = "Apple, Banana, Kiwi";
+// const output = fruits.slice(7, 13)
+
