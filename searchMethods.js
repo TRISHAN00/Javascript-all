e87f9f9493e005a8d06689c96941439d86851899
TRISHAN00 <trishansaha43@gmail.com>
2021-12-13 16:50:32 +0600

    search method completed

--- searchMethods.js
+++ searchMethods.js
@@ -6,13 +6,31 @@ const str = "I love my country too much."
 const strLastIndexOf = "I am something telling something";
 const resultLstIndexOf = strLastIndexOf.lastIndexOf('something', 13)
 
+// console.log(strLastIndexOf.split(' '));
+
 // search method
 const searchMethod = "I am trying to searching to you.";
 const searchResult = searchMethod.includes('searchings')
 
 if( searchResult) {
-    return console.log('I have found');
+    // return console.log('I have found');
 }
 else {
-    console.log('not found');
+    // console.log('not found');
 }
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// let x = cars[1]
+
+// console.log(x);
+
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0] = "Ford"
+// console.log(cars);
+
+
+// remove last item of fruit
+const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.splice(0, 2)
+console.log(fruits);
