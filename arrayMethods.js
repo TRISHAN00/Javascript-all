eafdda9de4f47fadaa5c7e834b9dfcef309bed73
TRISHAN00 <trishansaha43@gmail.com>
2021-12-13 20:53:50 +0600

    forEach method

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,11 +1,15 @@
-const fruits = ['Banana', 'Orange', 'Apple', 'Mango']
 
-for (let i = 0; i < fruits.length; i++) {
-    const element = fruits[i];
-    const ul = document.getElementById('ul')
-    const li = document.createElement('li').textContent = element
+
+// for (let i = 0; i < fruits.length; i++) {
+//     const element = fruits[i];
+//     const ul = document.getElementById('ul')
+//     const li = document.createElement('li').textContent = element
    
-    ul.appendChild(li)
-}
-
-console.log('hello');
+//     ul.append(li)
+// }
+const fruits = ['Banana', 'Orange', 'Apple', 'Mango']
+fruits.forEach((item) => {
+    const ul = document.querySelector('#ul')
+    const li = document.createElement('li')
+    ul.append(li.textContent = item)
+})
