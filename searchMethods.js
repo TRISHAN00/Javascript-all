e8410e23fae32241be360a6781ec136f5169cb66
TRISHAN00 <trishansaha43@gmail.com>
2021-12-13 14:57:54 +0600

    lastIndexOf method

---
+++ searchMethods.js
@@ -0,0 +1,8 @@
+// indexOf search method
+const str = "I love my country too much."
+// console.log(str.indexOf('l'));
+
+// lastIndexOf
+const strLastIndexOf = "I am something telling something";
+const resultLstIndexOf = strLastIndexOf.lastIndexOf('something', 13)
+console.log(resultLstIndexOf);
