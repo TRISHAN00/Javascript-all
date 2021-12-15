ed6408945361d7056b1e34e9655257b6f4b08cda
TRISHAN00 <trishansaha43@gmail.com>
2021-12-15 22:48:49 +0600

    find last number of array

--- arraySort.js
+++ arraySort.js
@@ -1,8 +1,7 @@
 // Sort 
-const players = ['Banana', 'Apple', 'Juice', 'Orange']
+const players = ['Banana', 'Apple', 'Juice', 'Orange'];
 const sort = players.sort()
 const reverseSort = sort.reverse()
-
 
 // sorting number
 const number = [40, 100, 20, 30];
@@ -10,17 +9,18 @@ number.sort((a, b) => {
     return a - b
 });
 
-
-
 // sorting reverse
 const numberReverse = [40, 100, 20, 30];
 numberReverse.sort((a, b) => {
     return b - a
 })
 
-
 // randomly sort
 const randomSort = [40, 100, 20, 30];
 randomSort.sort((a, b) => {
     return 0.5 - Math.random();
 });
+
+// How to find last item of array
+const players1 = ['Banana', 'Apple', 'Juice', 'Orange', "Kiwi"];
+console.log(players1[players1.length - 1]);
