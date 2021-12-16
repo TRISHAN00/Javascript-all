5ea6df4e98b4ce21524c81e20020d8d08a8a8080
TRISHAN00 <trishansaha43@gmail.com>
2021-12-16 16:52:42 +0600

    filter method

--- arrayIteration.js
+++ arrayIteration.js
@@ -1,8 +1,19 @@
 // forEach Functiona
 const numbers = [45, 4, 9, 10, 7];
 
-numbers.forEach((value, index, array) => {
-    console.log(value);
-    console.log(index);
-    console.log(array);
+const newNumbers = numbers.map((value) => {
+    return value * 2
 })
+
+console.log(newNumbers);
+console.log(numbers);
+
+
+// numbers.forEach((value, index, array) => {
+//     console.log(value);
+//     console.log(index);
+//     console.log(array);
+// })
+
+
+// map functiona
