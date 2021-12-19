a85b9a4b076f218e5748a1af0ea320745b2b40d5
TRISHAN00 <trishansaha43@gmail.com>
2021-12-20 00:35:15 +0600

    replace with reg

--- regEx.js
+++ regEx.js
@@ -1,5 +1,14 @@
-// without regEx
+// // without regEx
+// const text = "Visit Bangladesh";
+// const findBangladesh = text.search("bangladesh");
+
+
+
+// using regEx
 const text = "Visit Bangladesh";
-const findBangladesh = text.search("bangladesh");
+
+// const findBangladesh = text.search(/bangladesh/i); //without replace
+const findBangladesh = text.replace(/bangladesh/i, "Australia"); //with replace
+
 
 console.log(findBangladesh);
