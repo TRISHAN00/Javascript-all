471dc0f929bb4176c65a49bccd3eceb4e82a0b67
TRISHAN00 <trishansaha43@gmail.com>
2021-12-26 21:33:17 +0600

    obj this method updated

--- objThis.js
+++ objThis.js
@@ -1,30 +1,38 @@
 const person = {
-    firstName : "Oushi",
-    lastName : "Saha",
-    totalSub : {
-        bangla : 75,
-        english : 85,
-        math : 65, 
-        science : 85
-    },
+  firstName: "Mithod",
+  lastName: "Saha",
+  gender: "male",
+  totalSub: {
+    bangla: 75,
+    english: 85,
+    math: 65,
+    science: 85,
+    abc: 75,
+  },
 
-    fullName : function () {
-        return this.firstName + " " + this.lastName
-    },
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 
-    totalMarks : function () {
-        let arrValue = Object.values(this.totalSub)
-        
-        const totalSum = arrValue.reduce((total, value) => {
-            return total + value
-        });
+  genderSwitch : function () {
+    return person.gender.value     
+  },
 
-        return totalSum
-    },
+  totalMarks: function () {
+    let arrValue = Object.values(this.totalSub);
 
-    fullDetailsOfStudents : function () {
-        return `The name our student is ${this.fullName()} and her total marks is ${this.totalMarks()}.`
-    }
-}
+    const totalSum = arrValue.reduce((total, value) => {
+      return total + value;
+    });
+
+    return totalSum;
+  },
+
+  fullDetailsOfStudents: function () {
+    return `The name of our student is ${this.fullName()} and his total marks is ${this.totalMarks()}.`;
+  },
+};
+
+
 
 console.log(person.fullDetailsOfStudents());
