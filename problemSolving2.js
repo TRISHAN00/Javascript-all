9cdd339883eb3c82e86563172a266ce36f1399fc
TRISHAN00 <trishansaha43@gmail.com>
2022-01-01 23:11:33 +0600

    e

--- problemSolving2.js
+++ problemSolving2.js
@@ -1,4 +1,7 @@
-const sentence = "I am trishan saha and i am from bangladesh. Trishan is my name"
+const sentence = "I am trishan saha and i am from bangladesh. Trishan is my name";
+
+console.log(sentence.split(' '));
 
 const matches = sentence.match(/trishan/ig)
 console.log(matches);
+
--- setMethods.js
+++ setMethods.js
@@ -12,7 +12,7 @@ letters.add(a)
 letters.add(b)
 
 
-console.log(letters);
+
 
 
 let textt = ''
@@ -20,4 +20,40 @@ letters.forEach((value) => {
     textt = textt + value
 })
 
-console.log(textt);
+// when we don't want to put multiple name in the array we can use set method.
+
+const names = new Set(['a', 'b', 'b', 'c', 'e'])
+// resulult with console is : Set(4) { 'a', 'b', 'c', 'e' }
+
+names.add('d')
+names.delete('e')
+// resulult with console is : Set(4) { 'a', 'b', 'c', 'd' } 
+
+
+// I can use forEach functioan in set method.
+names.forEach((value) => {
+    // return console.log(value + ':');
+})
+
+
+
+// ----
+const studentsNumber  = {
+    first : 0,
+    second : 1,
+    third : 3
+}
+
+
+for (const i in studentsNumber) {
+    console.log(i.value);
+}
+
+
+
+
+
+
+
+
+
