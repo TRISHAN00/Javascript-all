cc2b9aa58f54be4d0b410a3498cd464a255aff46
TRISHAN00 <trishansaha43@gmail.com>
2022-01-02 09:36:00 +0600

    for of loop in array

--- object.js
+++ object.js
@@ -10,8 +10,15 @@ for(let x in employers) {
     // console.log(employers[x]);
 }
 
-// for in use with array
+// for in use with array Note : do not use for in over array
 const numbers = [1, 45, 9, 10, 11]
 for (let number in numbers) {
-    console.log(numbers[number]);
+    // console.log(numbers[number]);
+}
+
+// for of
+const a = 'Trishan';
+
+for (const x of a) {
+    console.log(x);
 }
