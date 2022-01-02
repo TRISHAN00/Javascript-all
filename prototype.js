ec485b694a6d97bd4edd14608157adbc56b16211
TRISHAN00 <trishansaha43@gmail.com>
2022-01-02 10:40:04 +0600

    prototypee

---
+++ prototype.js
@@ -0,0 +1,14 @@
+function Person(firstName, lastName, age, country) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.age = age,
+    this.country = country,
+    this.fullName = function () {
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+Person.prototype.position = "Analyst";
+
+const sumit = new Person("Sumit", "Saha", 35, "Bangladesh")
+console.log(sumit.position);
