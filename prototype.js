3666ca401ff4431cd0516b5fbb7a50f7e95ddc37
TRISHAN00 <trishansaha43@gmail.com>
2022-01-02 10:45:33 +0600

    prototype another

--- prototype.js
+++ prototype.js
@@ -6,9 +6,9 @@ function Person(firstName, lastName, age, country) {
     this.fullName = function () {
         return this.firstName + ' ' + this.lastName
     }
-}
+};
 
 Person.prototype.position = "Analyst";
 
-const sumit = new Person("Sumit", "Saha", 35, "Bangladesh")
+const sumit = new Person("Sumit", "Saha", 35, "Bangladesh");
 console.log(sumit.position);
