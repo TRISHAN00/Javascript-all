6820b442411982ff2a68c9e2bddd5301715216d3
TRISHAN00 <trishansaha43@gmail.com>
2022-01-02 09:18:01 +0600

    for in with array method

--- object.js
+++ object.js
@@ -7,5 +7,11 @@ const employers = {
 }
 
 for(let x in employers) {
-    console.log(employers[x]);
+    // console.log(employers[x]);
+}
+
+// for in use with array
+const numbers = [1, 45, 9, 10, 11]
+for (let number in numbers) {
+    console.log(numbers[number]);
 }
