c60f7d6b2711af6534d952c3f52a6aea20a4c904
TRISHAN00 <trishansaha43@gmail.com>
2022-01-02 09:54:02 +0600

    how to use getter and setter externaly

--- accessors.js
+++ accessors.js
@@ -11,15 +11,25 @@
 // console.log(person.fullName);
 
 
+// const person = {
+//     firstName : 'Trishan',
+//     lastName : 'Saha',
+//     language : '',
+//     set lang(lang) {
+//         this.language = lang;
+//     }
+// };
+
 const person = {
     firstName : 'Trishan',
     lastName : 'Saha',
     language : '',
-    set lang(lang) {
-        this.language = lang;
-    }
 };
 
-person.lang = 'BD'
+Object.defineProperty(person, 'fullName', {
+    get : function () {
+        return this.firstName + ' ' + this.lastName
+    }
+}) 
 
 console.log(person);
