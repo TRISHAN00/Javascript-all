81a9a5c53f8eb6c34ca58f6d4a2190d5e52622fa
TRISHAN00 <trishansaha43@gmail.com>
2022-01-10 11:46:36 +0600

    synchronous way

--- async.js
+++ async.js
@@ -1,1 +1,10 @@
-console.log('asy');
+// Javascript read line by line order they maintain sequentially. It's call synchronous way. By default JavaScript Synchronous.
+function hello() {
+    console.log('Line 2');
+}
+
+console.log('Line 1');
+hello();
+console.log('Line 3');
+
+// But, javaScript can maintain with asynchronous way.
--- callback.js
+++ callback.js
@@ -1,24 +1,55 @@
-// call functiona according to order
+// call function according to order
 const mySecondFunc = () => {
     return console.log('second func');
 }
 
 const myFristFunc = () => {
-    return console.log('fistFunchtion');
+    // return console.log('fistFunchtion');
 }
 
 
 // myFristFunc()
 // mySecondFunc()
 
-// make functiona to follow sequence and these are the best practice.
+// make function to follow sequence and these are the best practice.
 const myCalculator = (a, b) => {
     return sum = a + b
 }
 
 const displayCalculator = (result) => {
-    return console.log(result);
+    // console.log(result);
 }
 
 const result = myCalculator(5, 1)
 displayCalculator(result)
+
+
+// another way of sequentially
+const display = (result) => {
+    // console.log(result);
+}
+
+const calculator = (a, b) => {
+    let sum = a + b
+    return display(sum)
+}
+
+calculator(5, 1)
+
+// but, there is a problem that they are dependent each other and do not have freedom.
+
+const displayAvr = (some) => {
+    console.log(some);
+}
+
+const calculateAvr = (a, b, c, callback) => {
+    let avr = a + b + c
+    if (callback) callback(avr)
+    return avr
+}
+
+const result = calculateAvr(1, 2, 3, displayAvr);
+console.log(result);
+
+
+
