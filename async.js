4a5344d8c47727e5a8d36b5206b2075b924ae9c8
TRISHAN00 <trishansaha43@gmail.com>
2022-01-10 11:52:12 +0600

    describe asynchronous javascript

--- async.js
+++ async.js
@@ -1,10 +1,15 @@
 // Javascript read line by line order they maintain sequentially. It's call synchronous way. By default JavaScript Synchronous.
 function hello() {
-    console.log('Line 2');
+    console.log('Line 2 Code');
 }
 
-console.log('Line 1');
-hello();
-console.log('Line 3');
+console.log('Line 1 Code');
+
+setTimeout(() => {
+    hello();
+}, 2000);
+
+
+console.log('Line 3 Code');
 
 // But, javaScript can maintain with asynchronous way.
