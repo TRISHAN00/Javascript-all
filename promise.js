44c4cc7e562e3fed17c6a603a197ee0c038092c2
TRISHAN00 <trishansaha43@gmail.com>
2022-01-16 12:44:28 +0600

    js promise

--- promise.js
+++ promise.js
@@ -4,23 +4,22 @@ console.log("Task 1");
 
 // Promise pattern
 const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(taskStatus) {
-            resolve("Task 2")
-        }
-        else {
-            reject("Failed!")
-        }
-    }, 2000);
+  setTimeout(() => {
+    if (taskStatus) {
+      resolve("Task 2");
+    } else {
+      reject("Failed!");
+    }
+  }, 2000);
 });
 
 console.log("Task 3");
 
 // call promise
 promise
-    .then((value) => {
-        console.log(value);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
